44eb27887b9dfc80d5c441894967ef0fbd6751a0
Tim Mattison <tim@mattison.org>
2023-12-12 10:11:45 -0500

    Added functions to get the MQTT configuration (signature and behavior of AwsIotWsUrl unchanged)
    v0.3.1

--- aws-iot-core-websockets.go
+++ aws-iot-core-websockets.go
@@ -5,8 +5,12 @@ import (
 	"context"
 	"crypto/hmac"
 	"crypto/sha256"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/iot"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"net/url"
 	"strings"
 	"time"
@@ -20,6 +24,46 @@ type IotWsConfig struct {
 const (
 	emptyStringHash = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 )
+
+// AwsIotWsMqttOptions creates a new MQTT client options object for connecting to AWS IoT Core via WebSockets.
+// The options object will have the broker URL and TLS config set but not the client ID. The TLS config will
+// utilize the certificate pool passed into it but will not use the AWS root CA certificate automatically.
+func AwsIotWsMqttOptionsCustom(ctx context.Context, iotWsConfig IotWsConfig, certificatePool *x509.CertPool) (*mqtt.ClientOptions, error) {
+	if iotWsConfig.Endpoint == "" {
+		endpoint, err := getEndpoint(ctx, iotWsConfig.AwsConfig)
+
+		if err != nil {
+			return nil, fmt.Errorf("could not get endpoint. [%s]", err.Error())
+		}
+
+		iotWsConfig.Endpoint = endpoint
+	}
+
+	wsUrl, err := AwsIotWsUrl(ctx, iotWsConfig)
+
+	if err != nil {
+		return nil, fmt.Errorf("could not get WebSocket URL. [%s]", err.Error())
+	}
+
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(wsUrl)
+	opts.SetTLSConfig(&tls.Config{RootCAs: certificatePool})
+
+	return opts, nil
+}
+
+// AwsIotWsMqttOptions creates a new MQTT client options object for connecting to AWS IoT Core via WebSockets.
+// The options object will have the broker URL and TLS config set but not the client ID. The TLS config will
+// utilize the AWS root CA certificate automatically.
+func AwsIotWsMqttOptions(ctx context.Context, iotWsConfig IotWsConfig) (*mqtt.ClientOptions, error) {
+	certificatePool, err := createCertificatePool()
+
+	if err != nil {
+		return nil, fmt.Errorf("could not create certificate pool. [%s]", err.Error())
+	}
+
+	return AwsIotWsMqttOptionsCustom(ctx, iotWsConfig, certificatePool)
+}
 
 func AwsIotWsUrl(ctx context.Context, iotWsConfig IotWsConfig) (string, error) {
 	credentials, err := iotWsConfig.AwsConfig.Credentials.Retrieve(ctx)
@@ -109,4 +153,50 @@ func awsSha(in string) string {
 	h := sha256.New()
 	h.Write([]byte(in))
 	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+// rootCa This is the root CA certificate from https://www.amazontrust.com/repository/AmazonRootCA1.pem
+const rootCa = `-----BEGIN CERTIFICATE-----
+MIIDQTCCAimgAwIBAgITBmyfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF
+ADA5MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6
+b24gUm9vdCBDQSAxMB4XDTE1MDUyNjAwMDAwMFoXDTM4MDExNzAwMDAwMFowOTEL
+MAkGA1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJv
+b3QgQ0EgMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALJ4gHHKeNXj
+ca9HgFB0fW7Y14h29Jlo91ghYPl0hAEvrAIthtOgQ3pOsqTQNroBvo3bSMgHFzZM
+9O6II8c+6zf1tRn4SWiw3te5djgdYZ6k/oI2peVKVuRF4fn9tBb6dNqcmzU5L/qw
+IFAGbHrQgLKm+a/sRxmPUDgH3KKHOVj4utWp+UhnMJbulHheb4mjUcAwhmahRWa6
+VOujw5H5SNz/0egwLX0tdHA114gk957EWW67c4cX8jJGKLhD+rcdqsq08p8kDi1L
+93FcXmn/6pUCyziKrlA4b9v7LWIbxcceVOF34GfID5yHI9Y/QCB/IIDEgEw+OyQm
+jgSubJrIqg0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMC
+AYYwHQYDVR0OBBYEFIQYzIU07LwMlJQuCFmcx7IQTgoIMA0GCSqGSIb3DQEBCwUA
+A4IBAQCY8jdaQZChGsV2USggNiMOruYou6r4lK5IpDB/G/wkjUu0yKGX9rbxenDI
+U5PMCCjjmCXPI6T53iHTfIUJrU6adTrCC2qJeHZERxhlbI1Bjjt/msv0tadQ1wUs
+N+gDS63pYaACbvXy8MWy7Vu33PqUXHeeE6V/Uq2V8viTO96LXFvKWlJbYK8U90vv
+o/ufQJVtMVT8QtPHRh8jrdkPSHCa2XV4cdFyQzR1bldZwgJcJmApzyMZFo6IQ6XU
+5MsI+yMRQ+hDKXJioaldXgjUkK642M4UwtBV8ob2xJNDd2ZhwLnoQdeXeGADbkpy
+rqXRfboQnoZsG4q5WTP468SQvvG5
+-----END CERTIFICATE-----`
+
+func createCertificatePool() (*x509.CertPool, error) {
+	certificatePool := x509.NewCertPool()
+
+	if ok := certificatePool.AppendCertsFromPEM([]byte(rootCa)); !ok {
+		return nil, fmt.Errorf("failed to add root CA certificate to certificate pool")
+	}
+
+	return certificatePool, nil
+}
+
+func getEndpoint(ctx context.Context, cfg aws.Config) (string, error) {
+	client := iot.NewFromConfig(cfg)
+
+	endpoint, err := client.DescribeEndpoint(ctx, &iot.DescribeEndpointInput{
+		EndpointType: aws.String("iot:Data-ATS"),
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return *endpoint.EndpointAddress, nil
 }
--- aws-iot-core-websockets_test.go
+++ aws-iot-core-websockets_test.go
@@ -2,12 +2,7 @@ package aws_iot_core_websockets_go
 
 import (
 	"context"
-	"crypto/tls"
-	"crypto/x509"
-	"fmt"
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/iot"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"testing"
@@ -30,26 +25,6 @@ func TestCredentialsAreValid(t *testing.T) {
 
 	if err != nil {
 		t.Errorf("Could not authenticate to AWS APIs. This is likely due to credentials missing or having expired. [%s]", err.Error())
-	}
-}
-
-func TestGetEndpoint(t *testing.T) {
-	ctx := context.Background()
-
-	cfg, err := config.LoadDefaultConfig(ctx)
-
-	if err != nil {
-		panic("configuration error, " + err.Error())
-	}
-
-	endpoint, err := getEndpoint(ctx, cfg)
-
-	if err != nil {
-		t.Errorf("Could not get endpoint. [%s]", err.Error())
-	}
-
-	if endpoint == "" {
-		t.Errorf("Endpoint was empty.")
 	}
 }
 
@@ -104,21 +79,12 @@ func TestWebSocketConnect(t *testing.T) {
 		Endpoint:  endpoint,
 	}
 
-	wsUrl, err := AwsIotWsUrl(ctx, iotWsConfig)
+	opts, err := AwsIotWsMqttOptions(ctx, iotWsConfig)
 
 	if err != nil {
-		t.Errorf("Could not get WebSocket URL. [%s]", err.Error())
+		t.Errorf("Could not get MQTT config. [%s]", err.Error())
 	}
 
-	certificatePool, err := createCertificatePool()
-
-	if err != nil {
-		t.Errorf("Could not create certificate pool. [%s]", err.Error())
-	}
-
-	opts := mqtt.NewClientOptions()
-	opts.AddBroker(wsUrl)
-	opts.SetTLSConfig(&tls.Config{RootCAs: certificatePool})
 	opts.SetClientID("test")
 
 	client := mqtt.NewClient(opts)
@@ -128,50 +94,4 @@ func TestWebSocketConnect(t *testing.T) {
 	if token.Wait() && token.Error() != nil {
 		t.Errorf("Could not connect to WebSocket URL. [%s]", token.Error())
 	}
-}
-
-// This is the root CA certificate from https://www.amazontrust.com/repository/AmazonRootCA1.pem
-const RootCa = `-----BEGIN CERTIFICATE-----
-MIIDQTCCAimgAwIBAgITBmyfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF
-ADA5MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6
-b24gUm9vdCBDQSAxMB4XDTE1MDUyNjAwMDAwMFoXDTM4MDExNzAwMDAwMFowOTEL
-MAkGA1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJv
-b3QgQ0EgMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALJ4gHHKeNXj
-ca9HgFB0fW7Y14h29Jlo91ghYPl0hAEvrAIthtOgQ3pOsqTQNroBvo3bSMgHFzZM
-9O6II8c+6zf1tRn4SWiw3te5djgdYZ6k/oI2peVKVuRF4fn9tBb6dNqcmzU5L/qw
-IFAGbHrQgLKm+a/sRxmPUDgH3KKHOVj4utWp+UhnMJbulHheb4mjUcAwhmahRWa6
-VOujw5H5SNz/0egwLX0tdHA114gk957EWW67c4cX8jJGKLhD+rcdqsq08p8kDi1L
-93FcXmn/6pUCyziKrlA4b9v7LWIbxcceVOF34GfID5yHI9Y/QCB/IIDEgEw+OyQm
-jgSubJrIqg0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMC
-AYYwHQYDVR0OBBYEFIQYzIU07LwMlJQuCFmcx7IQTgoIMA0GCSqGSIb3DQEBCwUA
-A4IBAQCY8jdaQZChGsV2USggNiMOruYou6r4lK5IpDB/G/wkjUu0yKGX9rbxenDI
-U5PMCCjjmCXPI6T53iHTfIUJrU6adTrCC2qJeHZERxhlbI1Bjjt/msv0tadQ1wUs
-N+gDS63pYaACbvXy8MWy7Vu33PqUXHeeE6V/Uq2V8viTO96LXFvKWlJbYK8U90vv
-o/ufQJVtMVT8QtPHRh8jrdkPSHCa2XV4cdFyQzR1bldZwgJcJmApzyMZFo6IQ6XU
-5MsI+yMRQ+hDKXJioaldXgjUkK642M4UwtBV8ob2xJNDd2ZhwLnoQdeXeGADbkpy
-rqXRfboQnoZsG4q5WTP468SQvvG5
------END CERTIFICATE-----`
-
-func createCertificatePool() (*x509.CertPool, error) {
-	certificatePool := x509.NewCertPool()
-
-	if ok := certificatePool.AppendCertsFromPEM([]byte(RootCa)); !ok {
-		return nil, fmt.Errorf("failed to add root CA certificate to certificate pool")
-	}
-
-	return certificatePool, nil
-}
-
-func getEndpoint(ctx context.Context, cfg aws.Config) (string, error) {
-	client := iot.NewFromConfig(cfg)
-
-	endpoint, err := client.DescribeEndpoint(ctx, &iot.DescribeEndpointInput{
-		EndpointType: aws.String("iot:Data-ATS"),
-	})
-
-	if err != nil {
-		return "", err
-	}
-
-	return *endpoint.EndpointAddress, nil
 }
